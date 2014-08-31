0f0d87a0a06ef1fe29d1a7536e150eb1139cc49c
Stefan Brannfjell <braste10@nith.no>
2014-08-31 13:43:25 +0200

    Possible Fix #61 Soft deletion successfully tested and works on TFS 0.3 and 1.0

--- engine/function/users.php
+++ engine/function/users.php
@@ -304,7 +304,8 @@ function guild_new_leader($new_leader, $gid) {
 // Returns $gid of a guild leader($cid).
 function guild_leader_gid($leader) {
 	$leader = (int)$leader;
-	return mysql_select_single("SELECT `id` FROM `guilds` WHERE `ownerid`='$leader';")['id'];
+	$data = mysql_select_single("SELECT `id` FROM `guilds` WHERE `ownerid`='$leader';");
+	return ($data === false) ? false : $data['id'];
 }
 
 // Returns guild leader(charID) of a guild. (parameter: guild_ID)
