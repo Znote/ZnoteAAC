ab1816bb8401dbafc9a63eb27cf811f89448d090
Atte <atteputte3@gmail.com>
2014-09-02 18:35:32 +0300

    Update powergamers.lua

--- LUA/TFS_10/other.md/powergamers.lua
+++ LUA/TFS_10/other.md/powergamers.lua
@@ -6,6 +6,19 @@ function onThink(interval, lastExecution, thinkInterval)
 	return true
 end
 
-TFS 1.0 (globalevents.xml)
-	<!-- Power Gamers -->
-	<globalevent name="PowerGamers" interval="15000" script="powergamers.lua"/>
+-- TFS 1.0 (globalevents.xml)
+--	<!-- Power Gamers -->
+--	<globalevent name="PowerGamers" interval="15000" script="powergamers.lua"/>
+
+-- SQL  (remember to remove all -- before executing)--
+--ALTER TABLE `players` ADD `exphist_lastexp` BIGINT( 255 ) NOT NULL DEFAULT '0', 
+--ADD `exphist1` BIGINT( 255 ) NOT NULL DEFAULT '0', 
+--ADD `exphist2` BIGINT( 255 ) NOT NULL DEFAULT '0', 
+--ADD `exphist3` BIGINT( 255 ) NOT NULL DEFAULT '0', 
+--ADD `exphist4` BIGINT( 255 ) NOT NULL DEFAULT '0', 
+--ADD `exphist5` BIGINT( 255 ) NOT NULL DEFAULT '0', 
+--ADD `exphist6` BIGINT( 255 ) NOT NULL DEFAULT '0', 
+--ADD `exphist7` BIGINT( 255 ) NOT NULL DEFAULT '0', 
+
+-- after that execute --
+-- UPDATE `players` SET `exphist_lastexp`=`players`.`experience`  
