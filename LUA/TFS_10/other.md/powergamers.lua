38c0c00da6080a7de371896a23cee86988a30281
Atte <atteputte3@gmail.com>
2014-09-10 01:13:04 +0300

    Update powergamers.lua

--- LUA/TFS_10/other.md/powergamers.lua
+++ LUA/TFS_10/other.md/powergamers.lua
@@ -1,7 +1,7 @@
 function onThink(interval, lastExecution, thinkInterval)
 	if (tonumber(os.date("%d")) ~= getGlobalStorageValue(23856)) then
 		setGlobalStorageValue(23856, (tonumber(os.date("%d"))))
-		db.query("UPDATE `znote_players` `a` INNER JOIN `players` `b` ON `a`.`id`=`b`.`id` SET `a`.`exphist7`=`a`.`exphist6`,  `a`.`exphist6`=`a`.`exphist5`, `a`.`exphist5`=`a`.`exphist4`, `a`.`exphist4`=`a`.`exphist3`, `a`.`exphist3`=`a`.`exphist2`, `a`.`exphist2`=`a`.`exphist1`, `a`.`exphist1`=`b`.`experience`-`a`.`exphist_lastexp`, `a`.`exphist_lastexp`=`b`.`experience`;")
+		db.query("UPDATE `znote_players` `a` INNER JOIN `players` `b` ON `a`.`id`=`b`.`player_id` SET `a`.`exphist7`=`a`.`exphist6`,  `a`.`exphist6`=`a`.`exphist5`, `a`.`exphist5`=`a`.`exphist4`, `a`.`exphist4`=`a`.`exphist3`, `a`.`exphist3`=`a`.`exphist2`, `a`.`exphist2`=`a`.`exphist1`, `a`.`exphist1`=`b`.`experience`-`a`.`exphist_lastexp`, `a`.`exphist_lastexp`=`b`.`experience`;")
 	end
 	return true
 end
