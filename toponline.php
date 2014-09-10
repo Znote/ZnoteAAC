4b97c12edb7f7fff1b15cf3758440f1fdf5497c8
Atte <atteputte3@gmail.com>
2014-09-10 10:57:44 +0300

    Update toponline.php

--- toponline.php
+++ toponline.php
@@ -1,22 +1,24 @@
 <?php
 require_once 'engine/init.php';
 include 'layout/overall/header.php'; 
-$powergamers = $config['powergamers'];
-$limit = $powergamers['limit'];
+$toponline = $config['toponline'];
+$limit = $toponline['limit'];
 
-if ($config['powergamers_enabled'] === false) {
-echo 'Powergamers page has been disabled at config.php.';
+if ($config['toponline_enabled'] === false) {
+echo 'This page has been disabled at config.php.';
 include 'layout/overall/footer.php';
 	exit();
 }
-?>
-<style>
-#selectedP {
-text-decoration: underline
-}
-</style>
-	  <?php
 $type = $_GET['type'];
+
+
+function onlineTimeTotal($value)
+{
+	$hours = floor($value / 3600);
+	$value = $value - $hours * 3600;
+	$minutes = floor($value / 60);
+		return '<font color="black">'.$hours.'h '.$minutes.'m</font>';
+}
 
 function hours_and_minutes($value, $color = 1)
 {
@@ -37,45 +39,48 @@ function hours_and_minutes($value, $color = 1)
 if(empty($type))
 	$znotePlayers = mysql_select_multi('SELECT * FROM `znote_players` AS `z` JOIN `players` AS `p` WHERE `p`.`id`=`z`.`player_id` and `p`.`group_id` < 3 ORDER BY `onlinetimetoday` DESC LIMIT '.$limit);
 elseif($type == "sum")
-	$znotePlayers = mysql_select_multi('SELECT * FROM `znote_players` AS `z` JOIN `players` AS `p` WHERE `p`.`id`=`z`.`player_id` and `p`.`group_id` < 3 ORDER BY `onlinetime1`+`onlinetime2`+`onlinetime3`+`onlinetime4`+`onlinetime5`+`onlinetime6`+`onlinetime7`+`onlinetimetoday` DESC LIMIT ' . $limit);
-elseif($type >= 1 && $type <= 7)
+	$znotePlayers = mysql_select_multi('SELECT * FROM `znote_players` AS `z` JOIN `players` AS `p` WHERE `p`.`id`=`z`.`player_id` and `p`.`group_id` < 3 ORDER BY `z`.`onlinetimeall` DESC LIMIT '. $limit);
+elseif($type >= 1 && $type <= 4)
 	$znotePlayers = mysql_select_multi('SELECT * FROM `znote_players` AS `z` JOIN `players` AS `p` WHERE `p`.`id`=`z`.`player_id` and `p`.`group_id` < 3 ORDER BY `onlinetime' . (int) $type . '` DESC LIMIT '.$limit);
 	
-echo '<CENTER><H2>Ranking of powergamers</H2></CENTER>
+echo '<CENTER><H2>Most online on server name</H2></CENTER>
 <BR>
 <table class="table table-striped">
 		<td><center><b>#</b></center></td>
 		<td width="10%"><b>Name</b></td>';
 
 if($type == "sum")
-	echo '<TD bgcolor="red"><b><center><a href="?subtopic=onlinetime&type=sum">Total</a></center></B></TD>';
+	echo '<td ><center><b><center><a href="?subtopic=onlinetime&type=sum">Total</a></center></B></TD>';
 else
-	echo '<TD bgcolor="yellow"><b><center><a href="?subtopic=onlinetime&type=sum">Total</a></center></B></TD>';
+	echo '<td ><center><b><center><a href="?subtopic=onlinetime&type=sum">Total</a></center></B></TD>';
 for($i = 3; $i >= 2; $i--)
 {
 	if($type == $i)
-		echo '<TD bgcolor="red"><b><center><a href="?subtopic=onlinetime&type='.$i.'">'.$i.' Days Ago</a></center></B></TD>';
+		echo '<TD ><b><center><a href="?subtopic=onlinetime&type='.$i.'">'.$i.' Days Ago</a></center></B></TD>';
 	else
-		echo '<TD bgcolor="yellow"><b><center><a href="?subtopic=onlinetime&type='.$i.'">'.$i.' Days Ago</a></center></B></TD>';
+		echo '<TD ><b><center><a href="?subtopic=onlinetime&type='.$i.'">'.$i.' Days Ago</a></center></B></TD>';
 }
 if($type == 1)
-	echo '<TD bgcolor="red"><b><center><a href="?subtopic=onlinetime&type=1">1 Day Ago</a></center></B></TD>';
+	echo '<TD ><b><center><a href="?subtopic=onlinetime&type=1">1 Day Ago</a></center></B></TD>';
 else
-	echo '<TD bgcolor="yellow"><b><center><a href="?subtopic=onlinetime&type=1">1 Day Ago</a></center></B></TD>';
+	echo '<TD ><b><center><a href="?subtopic=onlinetime&type=1">1 Day Ago</a></center></B></TD>';
 if(empty($type))
-	echo '<TD bgcolor="red"><b><center><a href="?subtopic=onlinetime">Today</a></center></B></TD>';
+	echo '<TD><b><center><a href="?subtopic=onlinetime">Today</a></center></B></TD>';
 else
-	echo '<TD bgcolor="yellow"><b><center><a href="?subtopic=onlinetime">Today</a></center></B></TD></tr>';
+	echo '<TD ><b><center><a href="?subtopic=onlinetime">Today</a></center></B></TD>';
+echo '</TR>';
+
 
 $number_of_rows = 1;
 if($znotePlayers)
 foreach($znotePlayers as $player)
 {	
 	echo '<td><center>'. $number_of_rows . '.</center></td>';
-		echo '<td><a href="characterprofile.php?name=' .$player['name']. '">' .$player['name']. '</a>';
+	echo '<td><a href="characterprofile.php?name=' .$player['name']. '">' .$player['name']. '</a>';
 	echo '<br> ' .$player['level']. ' '.htmlspecialchars(vocation_id_to_name($player['vocation'])).' ';
-	echo '<td align="right">'.hours_and_minutes($player['onlinetime1'] + $player['onlinetime2'] + $player['onlinetime3'] + $player['onlinetimetoday'], 0).'</td>';
-	echo '<td align="right">'.hours_and_minutes($player['onlinetime3']).'</td><td align="right">'.hours_and_minutes($player['onlinetime2']).'</td><td align="right">'.hours_and_minutes($player['onlinetime1']).'</td><td align="right">'.hours_and_minutes($player['onlinetimetoday']).'</td></tr>';
+	echo '<td ><center>' .onlineTimeTotal($player['onlinetimeall']).'</td>';
+	$number_of_rows++;
+	echo '<td ><center>'.hours_and_minutes($player['onlinetime3']).'</center></td><td ><center>'.hours_and_minutes($player['onlinetime2']).'</center></td><td ><center>'.hours_and_minutes($player['onlinetime1']).'</center></td><td ><center>'.hours_and_minutes($player['onlinetimetoday']).'</center></td></tr>';
 }
 echo '</TABLE></div>';
 ?>
