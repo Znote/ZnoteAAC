dc4dc8f77a50fe5e82286156c1e91faa92f68f18
Webus <igames241@gmail.com>
2017-01-09 13:00:12 +0100

    Add google reCAPTCHA

--- layout/head.php
+++ layout/head.php
@@ -6,4 +6,5 @@
   <link rel="stylesheet" type="text/css" href="layout/css/style.css" />
   <!-- modernizr enables HTML5 elements and feature detects -->
   <script type="text/javascript" src="layout/js/modernizr-1.5.min.js"></script>
+  <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
