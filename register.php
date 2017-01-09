7ec975ad967271b1f3e25e9e283cf750afddc5e8
Webus <igames241@gmail.com>
2017-01-09 12:58:35 +0100

    Add Google recaptcha

--- register.php
+++ register.php
@@ -28,6 +28,24 @@ if (empty($_POST) === false) {
 			}
 		}
 		
+		if(isset($_POST['g-recaptcha-response'])){
+          $captcha=$_POST['g-recaptcha-response'];
+        }
+		
+		if(!$captcha){
+          echo '<h2>Please check the the captcha form.</h2>';
+          exit;
+        }
+        $secretKey = "Put your secret key here";
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
+        $responseKeys = json_decode($response,true);
+        if(intval($responseKeys["success"]) !== 1) {
+          echo '<h2>You are spammer ! Get the @$%K out</h2>';
+        } else {
+          echo '<h2>Thanks for posting comment.</h2>';
+        }
+				
 		if (user_exist($_POST['username']) === true) {
 			$errors[] = 'Sorry, that username already exist.';
 		}
@@ -197,6 +215,9 @@ if (isset($_GET['success']) && empty($_GET['success'])) {
 			?>
 			<li>
 				<input type="submit" value="Create Account">
+			</li>
+			<li>
+				 <div class="g-recaptcha" data-sitekey="=== Your public site key ==="></div>
 			</li>
 		</ul>
 	</form>
