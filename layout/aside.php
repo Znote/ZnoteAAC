a301007c4668f8105fa38bf9a4514df1e9d242ea
Elime1 <Elime1@users.noreply.github.com>
2019-01-24 18:29:06 +0100

    Convert to tabs

--- layout/aside.php
+++ layout/aside.php
@@ -5,9 +5,9 @@
 		} else {
 			include 'layout/widgets/login.php';
 		}
-        if (user_logged_in() && is_admin($user_data)) include 'layout/widgets/Wadmin.php';
-        if ($config['otservers_eu_voting']['enabled']) include 'layout/widgets/vote.php';
-        include 'layout/widgets/charactersearch.php';
+		if (user_logged_in() && is_admin($user_data)) include 'layout/widgets/Wadmin.php';
+		if ($config['otservers_eu_voting']['enabled']) include 'layout/widgets/vote.php';
+		include 'layout/widgets/charactersearch.php';
 		include 'layout/widgets/topplayers.php';
 		include 'layout/widgets/highscore.php';
 		include 'layout/widgets/serverinfo.php';
