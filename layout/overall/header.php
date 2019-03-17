effe5d2b8e8a38050f2cc8db1f010c153516a2ca
Znote <stefan_brannfjell@live.no>
2019-03-17 01:41:03 +0100

    Adding page specific class to layout body

--- layout/overall/header.php
+++ layout/overall/header.php
@@ -6,7 +6,7 @@
 	$time = $time[1] + $time[0];
 	$start = $time;
 include 'layout/head.php'; ?>
-<body>
+<body<?php if (isset($page_filename) && strlen($page_filename) > 0) echo " class='page_{$page_filename}'"; ?>>
   <div id="main">
     <?php include 'layout/header.php'; ?>
     <div id="site_content">
