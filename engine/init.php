d8f22f9c331359c73dd7504e32791c9f15e36abe
Znote <stefan_brannfjell@live.no>
2019-10-22 03:13:32 +0200

    Initialize active_email in init.php. Forgot to include it in the previous update.

--- engine/init.php
+++ engine/init.php
@@ -56,7 +56,7 @@ if (user_logged_in() === true) {
 		$user_data = user_data($session_user_id, 'id', 'name', 'password', 'email', 'premdays');
 	else
 		$user_data = user_data($session_user_id, 'id', 'password', 'email', 'premend');
-	$user_znote_data = user_znote_account_data($session_user_id, 'ip', 'created', 'points', 'cooldown', 'flag');
+	$user_znote_data = user_znote_account_data($session_user_id, 'ip', 'created', 'points', 'cooldown', 'flag' ,'active_email');
 }
 $errors = array();
 // Log IP
