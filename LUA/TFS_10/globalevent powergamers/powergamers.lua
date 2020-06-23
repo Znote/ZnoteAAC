5f87fd23769da321e2b76190c64908885656aa56
Evil Puncker <EPuncker@users.noreply.github.com>
2020-06-23 06:56:06 -0300

    Added result.free to getEternalStorage (#422)

--- LUA/TFS_10/globalevent powergamers/powergamers.lua
+++ LUA/TFS_10/globalevent powergamers/powergamers.lua
@@ -10,6 +10,7 @@ local function getEternalStorage(key, parser)
 			return -1
 		end
 	end
+	result.free(value)
 	return tonumber(value) or value
 end
 
@@ -45,7 +46,7 @@ ADD `onlinetimeall` BIGINT(20) NOT NULL DEFAULT '0';
 
 -- after that execute: --
 --[[
-UPDATE `znote_players` AS `z` INNER JOIN  `players` AS `p` ON `p`.`id`=`z`.`player_id` SET `z`.`exphist_lastexp`=`p`.`experience`;
+UPDATE `znote_players` AS `z` INNER JOIN `players` AS `p` ON `p`.`id`=`z`.`player_id` SET `z`.`exphist_lastexp`=`p`.`experience`;
 ]]--
 
 -- TFS 1.X (data/globalevents.xml)
@@ -56,7 +57,7 @@ function onThink(interval, lastExecution, thinkInterval)
 	if tonumber(os.date("%d")) ~= getEternalStorage(23856) then
 		setEternalStorage(23856, (tonumber(os.date("%d"))))
 		db.query("UPDATE `znote_players` SET `onlinetime7`=`onlinetime6`, `onlinetime6`=`onlinetime5`, `onlinetime5`=`onlinetime4`, `onlinetime4`=`onlinetime3`, `onlinetime3`=`onlinetime2`, `onlinetime2`=`onlinetime1`, `onlinetime1`=`onlinetimetoday`, `onlinetimetoday`=0;")
-		db.query("UPDATE `znote_players` `z` INNER JOIN `players` `p` ON `p`.`id`=`z`.`player_id` SET `z`.`exphist7`=`z`.`exphist6`,  `z`.`exphist6`=`z`.`exphist5`, `z`.`exphist5`=`z`.`exphist4`, `z`.`exphist4`=`z`.`exphist3`, `z`.`exphist3`=`z`.`exphist2`, `z`.`exphist2`=`z`.`exphist1`, `z`.`exphist1`=`p`.`experience`-`z`.`exphist_lastexp`, `z`.`exphist_lastexp`=`p`.`experience`;")
+		db.query("UPDATE `znote_players` `z` INNER JOIN `players` `p` ON `p`.`id`=`z`.`player_id` SET `z`.`exphist7`=`z`.`exphist6`, `z`.`exphist6`=`z`.`exphist5`, `z`.`exphist5`=`z`.`exphist4`, `z`.`exphist4`=`z`.`exphist3`, `z`.`exphist3`=`z`.`exphist2`, `z`.`exphist2`=`z`.`exphist1`, `z`.`exphist1`=`p`.`experience`-`z`.`exphist_lastexp`, `z`.`exphist_lastexp`=`p`.`experience`;")
 	end
 	db.query("UPDATE `znote_players` SET `onlinetimetoday` = `onlinetimetoday` + 60, `onlinetimeall` = `onlinetimeall` + 60 WHERE `player_id` IN (SELECT `player_id` FROM `players_online` WHERE `players_online`.`player_id` = `znote_players`.`player_id`)")
 	return true
