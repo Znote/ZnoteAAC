a531a18fad4c525879d6070330c4bfd6b7fee3da
Michał "Gubihe <korejwo.michal@gmail.com>
2022-01-21 13:23:05 +0100

    Typo in footer.php (#504)
    Fixed bug introduced in https://github.com/Znote/ZnoteAAC/pull/502

--- engine/footer.php
+++ engine/footer.php
@@ -2,7 +2,6 @@
 	&copy; Znote AAC.
 	<?php
 	$finish = microtime(true);
-	$finish = $time;
 	$total_time = round(($finish - $start), 4);
 	echo 'Server date and clock is: '. getClock(false, true) .' Page generated in '. $total_time .' seconds. ';
 	?>
