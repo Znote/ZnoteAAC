Znote/ZnoteAAC

commit 6d402cef28545b859590df0e7cbe7edc2acb6e2f

microtime as float (#502)

engine/footer.php
@@ -1,9 +1,7 @@
 <footer>
 	&copy; Znote AAC.
 	<?php
-	$time = microtime();
-	$time = explode(' ', $time);
-	$time = $time[1] + $time[0];
+	$finish = microtime(true);
 	$finish = $time;
 	$total_time = round(($finish - $start), 4);
 	echo 'Server date and clock is: '. getClock(false, true) .' Page generated in '. $total_time .' seconds. ';

engine/database/connect.php
@@ -7,9 +7,8 @@ if (!function_exists("elapsedTime")) {
 		if ($l_start === false) global $l_start;
 		if ($l_time === false) global $l_time;
 
-		$l_time = explode(' ', microtime());
-		$l_finish = $l_time[1] + $l_time[0];
-		return round(($l_finish - $l_start), 4);
+		$l_time = microtime(true);
+		return round(($l_time - $l_start), 4);
 	}
 }
 

engine/init.php
@@ -1,8 +1,6 @@
 <?php if (version_compare(phpversion(), '5.6', '<')) die('PHP version 5.6 or higher is required.');
 
-$l_time = microtime();
-$l_time = explode(' ', $l_time);
-$l_time = $l_time[1] + $l_time[0];
+$l_time = microtime(true);
 $l_start = $l_time;
 
 function elapsedTime($l_start = false, $l_time = false) {

commit 10e39c79bf1f6d92f639299d913dd8f1ed587313

Typo in footer.php
Fixed bug introduced in https://github.com/Znote/ZnoteAAC/pull/502

engine/footer.php
@@ -2,7 +2,6 @@
 	&copy; Znote AAC.
 	<?php
 	$finish = microtime(true);
-	$finish = $time;
 	$total_time = round(($finish - $start), 4);
 	echo 'Server date and clock is: '. getClock(false, true) .' Page generated in '. $total_time .' seconds. ';
 	?>